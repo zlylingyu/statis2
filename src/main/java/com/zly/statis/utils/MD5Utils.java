ce840e849c34d2f7592de9714d8451bb5757451a
zhangshijian <zsj15170190970@163.com>
2021-12-28 13:38:17 +0800

    张时健：提交MD5加密工具类

---
+++ src/main/java/com/zly/statis/utils/MD5Utils.java
@@ -0,0 +1,18 @@
+package com.zly.statis.utils;
+
+import org.springframework.util.DigestUtils;
+
+public class MD5Utils {
+
+    //盐
+    private static final String salt = "MPC@)@!!@!%";
+
+    public static String getMD5(String string){
+        String val = string+salt;
+        return DigestUtils.md5DigestAsHex(val.getBytes());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMD5("123456"));
+    }
+}
