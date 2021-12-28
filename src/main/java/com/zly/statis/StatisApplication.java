b6c839afd3aec65d53dbaa336a20d514356133dc
Algorithm <zly133916@163.com>
2021-12-28 13:30:38 +0800

    Initial commit

---
+++ src/main/java/com/zly/statis/StatisApplication.java
@@ -0,0 +1,13 @@
+package com.zly.statis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zly/statis/StatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zly.statis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
